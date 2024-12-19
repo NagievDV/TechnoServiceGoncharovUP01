aaf9e26903abc179a004a02c015323a54ae591bb
Goncharov Dmitriy <gdmitry58@gmail.com>
2024-12-19 14:38:17 +0300

    UP01 by Goncharov

---
+++ TechnoServiceGoncharovUP01/TechEmployee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechnoServiceGoncharovUP01
+{
+    internal class TechEmployee
+    {
+        public static int positionID;
+    }
+}
